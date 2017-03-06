06d8541de0f5a4913427c0b6a7f639dc854f4e5a
Christopher Plantijn <cplantijn@accusoft.com>
2017-03-06 17:03:45 -0500

    removed dumn stuff

--- src/js/container/App.js
+++ src/js/container/App.js
@@ -5,9 +5,7 @@ import '../../styles/sample.scss';
 export default class Sample extends Component {
   render() {
     return (
-      <div className="tester">
-        <h1>Sup</h1>
-      </div>
+      <h1>Sup</h1>
     );
   }
 }
